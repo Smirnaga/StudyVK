241b6e2d290055228d33b2cfa16639a264f795e4
Smirnaga <smirnagakirill@gmail.com>
2020-03-06 14:47:10 +0200

    contacs Jquery

--- JQUERY ContactList/script.js
+++ JQUERY ContactList/script.js
@@ -2,151 +2,201 @@
 let $countFormElement = $('#countForm');
 let $listOfContactsElement = $('#listOfContacts');
 let $addInfoElement = $('#addInfo');
-let $nameElement = $('#name');
-let $surnameElement = $('#surname');
-let $emailElement = $('#email');
+const $inputs = $('.text');
+const $contactForm = $('#contactForm');
 const taskItemTemplate = $('#taskItemTemplate').html();
 const DELETE_BTN_CLASS = 'delete-btn';
+const CONTACT_ROW_CLASS = 'contact-row';
+const EMPTY_CONTACT = {
+    id: '',
+    name: '',
+    surname: '',
+    phone: '',
+    email: '',
+    date: ''
+};
+
+
+const $dialog = $('#dialog-form').dialog({
+    autoOpen: false,
+    height: 100,
+    width: 150,
+    modal: true,
+    buttons: {
+        Save: function() {
+            createContact();
+            $dialog.dialog('close');
+            
+        },
+        Cancel: function() {
+            $dialog.dialog('close');
+            
+        }
+    },
+    Close: function() {
+        resetForm();
+    }
+});
 
 
 const CONTACT_URL = 'http://5dd3d5ba8b5e080014dc4bfa.mockapi.io/contacts';
 
 let list = [];
 
+$('#date').datepicker();
+
+$contactForm.on('submit', e => {
+    e.preventDefault();
+    saveContact();
+});
+
+$addInfoElement.on('click', () => openModal(getEmptyContact()));
+$listOfContactsElement.on('click', '.edit-btn', onEditBtnClick);
+$listOfContactsElement.on('click', '.delete-btn', onDeleteBtnClick);
+
 init();
 
-function init (){
-    getUsers();
+function init() {
+    getData()
+        .then(setUsers)
+        .then(renderUsers);
 }
 
-function getUsers() {
-    return fetch(CONTACT_URL)
-    .then(resp => resp.json())
-    .then(setUsers)
-    .then(renderUsers);
+function onEditBtnClick(e) {
+    const id = getContactId(e.target);
+    editContact(id);
 }
 
-function setUsers(list){
-    return list ;
+function onDeleteBtnClick(e) {
+    const id = getContactId(e.target);
+    deleteContact(id);
+}
+
+function getContactId(el) {
+    return $(el)
+        .closest('.' + CONTACT_ROW_CLASS)
+        .data('id');
+}
+
+function getContactElement(id) {
+    return $listOfContactsElement.find(`[data-id="${id}"]`);
+}
+
+function getContact(id) {
+    return list.find(item => item.id == id);
+}
+
+function editContact(id) {
+    openModal(getContact(id));
 }
 
 
-function renderUsers(list) {
-    list = $.map(list,generateUsersHtml).join('\n') ;
-    $listOfContactsElement.append(list);
-    
+function getData() {
+    return fetch(CONTACT_URL).then(res => res.json());
 }
 
-function generateUsersHtml(list) {
-  return taskItemTemplate
-        .replace('{{id}}', list.id)
-        .replace('{{name}}', list.name)
-        .replace('{{surname}}', list.surname)
-        .replace('{{email}}', list.email)
-        .replace('{{phone}}', list.phone)
-        .replace('{{date}}', list.date);
-    
-    
+function setUsers(data) {
+    return (list = data);
 }
 
-$addInfoElement.on('click', addToBoard);
+function renderUsers(data) {
+    const html = data.map(getContactHtml).join('\n');
 
-function addToBoard(e) {
-    e.preventDefault();
-    console.log('clicked');
-    dialog.dialog('open');
+    $listOfContactsElement.html(html);
 }
 
-function initDialog() {
-    dialog = $('#dialog-form').dialog({
-        autoOpen: false,
-        height: 100,
-        width: 150,
-        modal: true,
-        buttons: {
-            Create: function() {
-                createSticker();
-                dialog.dialog('close');
-                $('.edit').val('');
-            },
-            Cancel: function() {
-                dialog.dialog('close');
-                $('.edit').val('');
-            }
-        },
-        close: function() {
-            $('.edit').val('');
-        }
+
+function getContactHtml(contact) {
+    return taskItemTemplate
+        .replace('{{id}}', contact.id)
+        .replace('{{name}}', contact.name)
+        .replace('{{surname}}', contact.surname)
+        .replace('{{email}}', contact.email)
+        .replace('{{phone}}', contact.phone)
+        .replace('{{date}}', formatDate(contact.date));
+}
+
+function formatDate(date) {
+    date = new Date(date);
+    return date.getDate() + '/' + date.getMonth() + '/' + date.getYear();
+}
+
+
+function getEmptyContact() {
+    return { ...EMPTY_CONTACT };
+}
+
+function openModal(contact) {
+    fillForm(contact);
+    $dialog.dialog('open');
+}
+
+function fillForm(list) {
+    $inputs.each((i, el) => {
+        el.value = list[el.name];
     });
 }
 
-
-
-
-/* $addInfoElement.on('click', onBtnClick);
-
-function onBtnClick() {
-    this.classList.add('loading');
-
-    setTimeout(() => {
-        this.classList.remove('loading');
-    }, 2000);
+function getContactElement(id) {
+    return $listOfContactsElement.find(`[data-id="${id}"]`);
 }
 
-$addInfoElement.on('click', addToTable);
+function saveContact() {
+    const contact = getFormData();
 
-function addToTable(e) {
-    e.preventDefault();
-    addRow();
-    resetForm();
-}
-
-
-function resetForm() {
-    countFormElement.reset();
-}
-
-function validate(value) {
-    return !!value.trim();
-}
-
-$listOfContactsElement.on('click', onListClick);
-
-function onListClick(e){
-    switch(true) {
-        case e.target.classList.contains(DELETE_BTN_CLASS):
-            deleteUsers(e.target.parentNode.dataset.id);
-            break;
+    if (contact.id) {
+        updateContact(contact);
+    } else {
+        createContact(contact);
     }
 }
 
-function deleteUsers(id){
-    const users = list.find(users => users.id === users.id);
-    fetch(`${CONTACT_URL}/${users.id}`, {
-        method: 'DELETE'
+function getFormData() {
+    let contact = {};
+    $inputs.each((i, el) => {
+        contact[el.name] = el.value;
     });
 
-    list = list.filter(users => users.id !== id);
+    return contact;
+}
 
-    renderUsers(list);
-} */
+function updateContact(contact) {
+    fetch(`${CONTACT_URL}/${contact.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contact)
+    });
 
-/* unction addRow() {
-    const users = { name: nameElement.value, surname: surnameElement.value, email: emailElement.value };
+    list = list.map(item => (item.id == contact.id ? contact : item));
+    getContactElement(contact.id).replaceWith(getContactHtml(contact));
+}
 
-    fetch(CONTACT_URL, {
+function createContact(contact) {
+    fetch(`${CONTACT_URL}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(users)
+        body: JSON.stringify(contact)
     })
-        .then(resp => resp.json())
-        .then(addUser);
+        .then(res => res.json())
+        .then(contact => {
+            list = [...list, contact];
+            $listOfContactsElement.append(getContactHtml(contact));
+        });
 }
 
-function addUser(users) {
-    list.push(users);
+function deleteContact(id) {
+    fetch(`${CONTACT_URL}/${id}`, {
+        method: 'DELETE'
+    });
 
-    renderUsers(list);
-}  */
+    getContactElement(id).remove();
+}
+
+function resetForm() {
+    $inputs.val('');
+}
+
