bc1c6379fc551638f5719208f3953d85693f4d87
Smirnaga <smirnagakirill@gmail.com>
2020-03-09 16:44:26 +0200

    gallery

--- Gallery/gulpfile.js
+++ Gallery/gulpfile.js
@@ -1,5 +1,7 @@
-const {series,src,dest} = require('gulp');
+const {series,src,dest,watch} = require('gulp');
 const concat = require('gulp-concat');
+const babel = require("gulp-babel");
+const sass = require('gulp-sass');
 
 function defaultTask(cb){
     console.log('Gulp is running');
@@ -13,17 +15,27 @@ function html() {
 function scripts(){
     console.log('scripts is running');
     return src('./src/**/*.js')
+        .pipe(babel())
         .pipe(concat('all.js'))
         .pipe(dest('./dist'));
+        
 }
-function styles(cb){
-    console.log('styles is running');
-    cb();
+function styles() {
+    console.log('building styles');
+    return src('./src/styles.sass')
+        .pipe(sass())
+        .pipe(dest('./dist'));
+}
+
+function watchFiles() {
+    watch('./src/**/*.js', scripts);
+    watch('./src/**/*.sass', styles);
 }
 
 
 
 module.exports = {
     default: defaultTask,
-    build: series(html,scripts,styles)
+    build: series(html,scripts,styles),
+    dev: series(html,scripts,styles,watchFiles)
 }
