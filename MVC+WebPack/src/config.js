9d1467df28cee9e7a5b369ff7cd938fe67344c20
Smirnaga <smirnagakirill@gmail.com>
2020-03-23 19:20:53 +0200

    MVC

--- MVC+WebPack/src/config.js
+++ MVC+WebPack/src/config.js
@@ -1,2 +1,2 @@
 export const TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
-export const taskItemTemplate = document.getElementById('taskItemTemplate');
+
--- MVC+WebPack/src/controller/Controller.js
+++ MVC+WebPack/src/controller/Controller.js
@@ -1,11 +1,13 @@
 import Collection from '../model/Collection';
 import List from '../view/List';
+import FormView from '../view/form';
 
 export default class Controller {
     constructor(){
         console.log('init');
 
         this.listView = new List();
+        this.formView = new FormView();
         this.container = document.getElementById('todos');
 
         this.todosCollection = new Collection();
@@ -17,6 +19,7 @@ export default class Controller {
        
 
         this.container.append(this.listView.el);
+        this.container.append(this.formView.el);
 
 
         
